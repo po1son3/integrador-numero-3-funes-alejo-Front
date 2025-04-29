fa596e67b49e13aec639c2ca8dec19a96a9f973c
Alejo Funes <alefunesrios@gmail.com>
2025-04-29 03:28:36 -0300

    agrego al main el carrito provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
 import { ProductosProvider } from './contexts/ProductosContext.jsx'
+import { CarritoProvider } from './contexts/CarritoContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<ProductosProvider>
-    <App />
-</ProductosProvider>  
+<CarritoProvider>
+    <ProductosProvider>
+      <App />
+    </ProductosProvider>
+  </CarritoProvider> 
   </StrictMode>,
 )
