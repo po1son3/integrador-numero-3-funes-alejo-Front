b225f5381caf10acff47bfa74ebb16582d17995b
Alejo Funes <alefunesrios@gmail.com>
2025-05-01 03:05:00 -0300

    estilizo la tabla del formulario tabla-jsx

--- src/components/components-alta/Tabla.jsx
+++ src/components/components-alta/Tabla.jsx
@@ -8,21 +8,21 @@ const Tabla = () => {
 const {productos} = useContext(ProductosContext)
 
   return (
-    <table className='tabla-alta'>
-    <thead>
-        <tr>
-            <th>Nombre</th>
-            <th>Precio</th>
-            <th>Stock</th>
-            <th>Marca</th>
-            <th>Categoría</th>
-            <th>Detalles</th>
-            <th>Foto</th>
-            <th>Envío</th>
-            <th>Acciones</th>
+    <table className='table'>
+    <thead className='table__encabezado'>
+        <tr className='table__fila'>
+            <th className='table__celda table__celda--encabezado'>Nombre</th>
+            <th className='table__celda table__celda--encabezado'>Precio</th>
+            <th className='table__celda table__celda--encabezado'>Stock</th>
+            <th className='table__celda table__celda--encabezado'>Marca</th>
+            <th className='table__celda table__celda--encabezado'>Categoría</th>
+            <th className='table__celda table__celda--encabezado'>Detalles</th>
+            <th className='table__celda table__celda--encabezado'>Foto</th>
+            <th className='table__celda table__celda--encabezado'>Envío</th>
+            <th className='table__celda table__celda--encabezado'>Acciones</th>
         </tr>
     </thead>
-    <tbody>
+    <tbody className='table__cuerpo'>
 
     {
         productos && productos.map((producto) => (
