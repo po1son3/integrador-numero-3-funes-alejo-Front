a130222294f2c933f19256719fd6d43facee550a
Alejo Funes <alefunesrios@gmail.com>
2025-05-01 01:06:41 -0300

    agrego el footer html react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,25 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+    <div className="lado-izquiero-lp">
+    Luxury Paradise  
+    </div>
+    <hr />
+    <div className="lado-derecho-content">
+    <div className="redes-icon">
+    <a href="https://facebook.com">Facebook</a> 
+    <a href="https://twitter.com">Twitter</a>
+    <a href="https://instagram.com">Instagram</a>
+    </div> 
+    <div className="nav-links-redirec">
+    <a href="/">Inicio</a>
+    <a href="/alta">Alta</a>
+    <a href="/nosotros">Nosotros</a>
+    <a href="/contacto">Contacto</a>
+    </div>
+    </div>
+</footer>
   )
 }
 
