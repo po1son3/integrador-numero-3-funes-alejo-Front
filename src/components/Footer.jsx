4cac7321cc0c8b13dea15fbbb5a329729e702232
Alejo Funes <alefunesrios@gmail.com>
2025-05-01 01:18:35 -0300

    cambio los a por link y href por to

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router"
 
 const Footer = () => {
   return (
@@ -8,19 +9,19 @@ const Footer = () => {
     <hr />
     <div className="lado-derecho-content">
     <div className="redes-icon">
-    <a href="https://facebook.com">Facebook</a> 
-    <a href="https://twitter.com">Twitter</a>
-    <a href="https://instagram.com">Instagram</a>
+    <Link to="https://facebook.com">Facebook</Link> 
+    <Link to="https://twitter.com">Twitter</Link>
+    <Link to="https://instagram.com">Instagram</Link>
     </div> 
     <div className="nav-links-redirec">
-    <a href="/">Inicio</a>
-    <a href="/alta">Alta</a>
-    <a href="/nosotros">Nosotros</a>
-    <a href="/contacto">Contacto</a>
+    <Link to="/">Inicio</Link>
+    <Link to="/alta">Alta</Link>
+    <Link to="/nosotros">Nosotros</Link>
+    <Link to="/contacto">Contacto</Link>
     </div>
     </div>
 </footer>
-  )
+)
 }
 
 export default Footer
