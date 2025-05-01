b2d6aa7b14f38f2f21fb296d0e3c9d48f3bff6bc
Alejo Funes <alefunesrios@gmail.com>
2025-05-01 04:07:28 -0300

    agrego nosotrosjsx y estilizo al 100

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -1,9 +1,18 @@
 import useTitulo from "../hooks/useTitulo"
-
+import './Nosotros.scss'
 const Nosotros = () => {
   useTitulo('Nosotros')
   return (
-    <div>Nosotros</div>
+  <main>
+  <img 
+    src="/public/imgs/luxury-paradise.webp" 
+    alt="tienda de ropa" 
+    className="imagen-centrada"
+  />
+  <p className="parrafo-arreglado">
+    Nosotros somos Luxury Paradise, una empresa que no solo se fija en que vistas bien, sino también en la calidad de nuestros productos y la experiencia.
+  </p>
+</main>
   )
 }
 
